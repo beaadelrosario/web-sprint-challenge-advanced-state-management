e5702d447e30f9907f5230d7f51d1f40658aa711
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-08-20 14:03:22 -0700

    styling

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Smurfs from "../components/Smurfs"
+import Header from "../components/Header"
 
 export default function App() {
     return (
       <div className="App">
+        <Header />
         <h1>Smurf App!</h1>
         <Smurfs />
       </div>
